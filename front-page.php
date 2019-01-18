bee7692a75b6b8b386cb781a6ccb895c3cee334c
Dillon Brickhouse <trombonecricket@gmail.com>
2019-01-18 12:36:20 -0700

    Re-introduced front-page.php with modifications for Gutenberg

---
+++ front-page.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Template Name: Home
+*/
+?>
+
+<!--Header-->
+
+<?php get_template_part( 'template-parts/page/content', 'header' ); ?>
+
+<!--End Header-->
+
+<!--Content-->
+
+<?php get_template_part( 'template-parts/internal/content', 'loop' ); ?>
+
+<!--End Content-->
+
+<!--Footer-->
+
+<?php get_template_part( 'template-parts/page/content', 'footer' ); ?>
+
+<!--End Footer-->
