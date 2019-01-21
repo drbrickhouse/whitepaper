cafb67e77dbad5b9eb702d96b48fcf60ff8e7ec0
Dillon Brickhouse <trombonecricket@gmail.com>
2019-01-21 10:15:07 -0700

    Created fallback so that theme will work if ACF is not active

--- template-parts/page/content-footer.php
+++ template-parts/page/content-footer.php
@@ -1,8 +1,14 @@
 <?php
-$footer_choice = get_field('footer');
-if ($header_footer == 'default') {
-  get_footer();
+
+if( is_plugin_active('advanced-custom-fields/acf.php') ) {
+  $footer_choice = get_field('footer');
+  if ($header_footer == 'default') {
+    get_footer();
+  } else {
+    get_footer($footer_choice);
+  }
 } else {
-  get_footer($footer_choice);
+  get_footer();
 }
+
 ?>
--- template-parts/page/content-header.php
+++ template-parts/page/content-header.php
@@ -1,8 +1,14 @@
 <?php
-$header_choice = get_field('header');
-if ($header_choice == 'default') {
-  get_header();
+
+if( is_plugin_active('advanced-custom-fields/acf.php') ) {
+  $header_choice = get_field('header');
+  if ($header_choice == 'default') {
+    get_header();
+  } else {
+    get_header($header_choice);
+  }
 } else {
-  get_header($header_choice);
+  get_header();
 }
+
 ?>
