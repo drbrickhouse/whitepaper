1fa653dae8ec0c5829b0fffcccef18ff63ceb694
Dillon Brickhouse <trombonecricket@gmail.com>
2019-01-21 10:26:06 -0700

    Breadcrumb container only shows if Yoast breadcrumbs are active

--- template-parts/internal/content-breadcrumbs.php
+++ template-parts/internal/content-breadcrumbs.php
@@ -1,7 +1,5 @@
-<div class="container-fluid" id="breadcrumb-wrapper">
-  <?php
-  if ( function_exists('yoast_breadcrumb') ) {
-  yoast_breadcrumb('<p id="breadcrumbs">','</p>');
-  }
-  ?>
-</div>
+<?php if ( function_exists('yoast_breadcrumb') ) { ?>
+  <div class="container-fluid" id="breadcrumb-wrapper">
+    <?php yoast_breadcrumb('<p id="breadcrumbs">','</p>'); ?>
+  </div>
+<?php } ?>
