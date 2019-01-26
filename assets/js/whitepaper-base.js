c4ba2ce530dfe6a5d937f8790749ee19cb1a4bac
Dillon Brickhouse <trombonecricket@gmail.com>
2019-01-26 12:52:07 -0700

    Made dropdown toggle appear no matter what the screen size is.

--- assets/js/whitepaper-base.js
+++ assets/js/whitepaper-base.js
@@ -1,10 +1,4 @@
 jQuery(function($) {
   // Make Bootstrap Menu Dropdowns Work on Mobile
-  $(window).resize(function() {
-    if($(window).width() < 767) {
-      $( '.dropdown-toggle' ).after( '<i class="fas fa-plus whitepaper-toggle" data-toggle="dropdown"></i>' );
-    } else {
-      $( '.whitepaper-toggle' ).remove();
-    }
-  }
+  $( '.dropdown-toggle' ).after( '<i class="fas fa-plus whitepaper-toggle" data-toggle="dropdown"></i>' );
 });
