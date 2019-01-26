114a1c25dcce37e4bd3c4899c8797059d3f24af8
Dillon Brickhouse <trombonecricket@gmail.com>
2019-01-26 12:34:19 -0700

    Set dropdown toggle to appear/disappear on window resize

--- assets/js/whitepaper-base.js
+++ assets/js/whitepaper-base.js
@@ -1,8 +1,10 @@
 jQuery(function($) {
   // Make Bootstrap Menu Dropdowns Work on Mobile
-  if($(window).width() < 767) {
-    $( '.dropdown-toggle' ).after( '<i class="fas fa-plus whitepaper-toggle" data-toggle="dropdown"></i>' );
-  } else {
-    $( '.whitepaper-toggle' ).remove();
+  $(window).resize(function() {
+    if($(window).width() < 767) {
+      $( '.dropdown-toggle' ).after( '<i class="fas fa-plus whitepaper-toggle" data-toggle="dropdown"></i>' );
+    } else {
+      $( '.whitepaper-toggle' ).remove();
+    }
   }
 });
