4305a72518b4544c66fb43a3befc099d50ac419e
Dillon Brickhouse <trombonecricket@gmail.com>
2019-01-26 10:09:48 -0700

    Set site credits to be on the left side on larger screens

--- template-parts/footer/content-copyright.php
+++ template-parts/footer/content-copyright.php
@@ -1,9 +1,9 @@
 <div class="container-fluid" id="copyright">
   <div class="row" id="copyright-inner">
-    <div class="col-12 col-sm-12 col-lg-6">
+    <div class="col-12 col-sm-12 col-lg-6 text-center text-lg-left">
       <p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?> <?php if(get_theme_mod('whitepaper_sitemap_link_on') == 'yes') { ?> | <a href="<?php echo get_theme_mod('whitepaper_sitemap_link') ?>" title="sitemap">Sitemap</a> <?php } ?></p>
     </div>
-    <div class="col-12 col-sm-12 col-lg-6">
+    <div class="col-12 col-sm-12 col-lg-6 text-center text-lg-right">
       <?php if(get_theme_mod('whitepaper_site_credits_on') == 'yes') { ?>
         <p>Site by: <a href="<?php echo get_theme_mod('whitepaper_site_credits_link') ?>"><img src="<?php echo get_theme_mod('whitepaper_site_credits_logo') ?>"></a>
       <?php } ?>
