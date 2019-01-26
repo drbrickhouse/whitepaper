a093af8126809b7e7da019cd18f11f82738f0871
Dillon Brickhouse <trombonecricket@gmail.com>
2019-01-26 12:31:52 -0700

    Removed dropdown toggler on desktop

--- assets/js/whitepaper-base.js
+++ assets/js/whitepaper-base.js
@@ -1,6 +1,8 @@
 jQuery(function($) {
   // Make Bootstrap Menu Dropdowns Work on Mobile
   if($(window).width() < 767) {
-     $( ".dropdown-toggle" ).after( '<i class="fas fa-plus text-right" data-toggle="dropdown"></i>' );
+    $( '.dropdown-toggle' ).after( '<i class="fas fa-plus whitepaper-toggle" data-toggle="dropdown"></i>' );
+  } else {
+    $( '.whitepaper-toggle' ).remove();
   }
 });
