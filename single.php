8fb47ed7aa25cbf536b99e9c449292b1087d6633
Dillon Brickhouse <trombonecricket@gmail.com>
2019-01-28 16:55:46 -0700

    Brought single.php into parity with new features.

--- single.php
+++ single.php
@@ -5,29 +5,28 @@
 <!--End Header-->
 
 <!--Content-->
-<div class="row">
-  <!--Breadcrumbs-->
-  <?php get_template_part( 'template-parts/internal/content', 'breadcrumbs' ); ?>
-  <!--End Breadcrumbs-->
-  <div class="col-12">
-    <div class="row" id="main-wrapper">
+
+<!--Breadcrumbs-->
+
+<?php get_template_part( 'template-parts/internal/content', 'breadcrumbs' ); ?>
+
+<!--End Breadcrumbs-->
+
+<main class="container">
+  <article>
+    <div class="row" id="title-bar">
       <div class="col-12">
-        <div class="row" id="title-bar">
-          <div class="col-12 col-lg-8 offset-lg-2">
-            <?php get_template_part( 'template-parts/internal/content', 'title' ); ?>
-          </div>
-        </div>
-        <div class="content-wrapper">
-          <div class="row">
-            <div class="col-12 col-lg-8 offset-lg-2">
-              <?php get_template_part( 'template-parts/internal/content', 'loop' ); ?>
-            </div>
-          </div>
-        </div>
+        <?php get_template_part( 'template-parts/internal/content', 'title' ); ?>
       </div>
     </div>
-  </div>
-</div>
+    <div class="row" id="content-wrapper">
+      <div class="col-12">
+        <?php get_template_part( 'template-parts/internal/content', 'loop' ); ?>
+      </div>
+    </div>
+  </article>
+</main>
+
 <!--End Content-->
 
 <!--Footer-->
